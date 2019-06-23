297a9135b05c6dd40a387a20488fea04b939ae9b
shadow <rezaking2022@gmail.com>
2019-06-23 11:11:16 +0430

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -9,3 +9,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.text, self.amount)
+
+class Incom(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.text, self.amount)
